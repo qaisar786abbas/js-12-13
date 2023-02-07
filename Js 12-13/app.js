959e58ea97e6488197c2c1fffad97c192cce896a
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-02-07 18:11:37 +0500

    added files

---
+++ Js 12-13/app.js
@@ -0,0 +1,172 @@
+//>>>>>>CHAPTER 11-13
+
+//Question no 01
+
+// var alphabet = "x";
+// var range1 = "A";
+// var range2 = "Z";
+// var range3 = "a";
+// var range4 = "z";
+// var alphabet = prompt("Enter the Alphabet?");
+// if(alphabet >= range1 && alphabet <= range2) {
+//     alert("The alphabet is uppercase.");
+// }
+// else if(alphabet >= range3 && alphabet <= range4) {
+//     alert("The alphabet is lowercase.");
+// }
+// else {
+//     alert("invalid");
+// }
+
+//Question no 2
+
+// var int1 = 8;
+// var int2 = 11;
+// var int1 = prompt("Enter the integer1?");
+// var int2 = prompt("Enter the integer2?");
+// if(int1 > int2) {
+//     alert(int1);
+// }
+// else if(int1 < int2) {
+//     alert(int2);
+// }
+// else {
+//     alert("Numbers are equal.");
+// }
+
+//Question no 03
+
+// var number = "x";
+// var number = prompt("Enter the number?");
+// if(number > 0) {
+//     alert("Number is positive.");
+// }
+// else if(number < 0) {
+//     alert("Number is negative.");
+// }
+// else {
+//    alert("Number is equal to 0.");
+// }
+
+//Question no 04
+
+// var character = "x";
+// var vowel1 = "a";
+// var vowel2 = "e";
+// var vowel3 = "i";
+// var vowel4 = "o";
+// var vowel5 = "u";
+// var character = prompt("Enter the character?");
+// if(character == vowel1) {
+//     alert("The character is vowel.");
+// }
+// else if(character == vowel2) {
+//     alert("The character is vowel.");
+// }
+// else if(character == vowel3) {
+//     alert("The character is vowel.");
+// }
+// else if(character == vowel4) {
+//     alert("The character is vowel.");
+// }
+// else if(character == vowel5) {
+//     alert("True! the character is vowel.");
+// }
+// else {
+//    alert("False! not a vowel.");
+// }
+
+//Question no 05
+
+// var password = "x";
+// var password1 = 54776;
+// var password = prompt("Enter the password?");
+// if(password == password1) {
+//     alert("“Correct! The password you entered matches the original password”.");
+// }
+// else {
+//     alert("Incorrect Password.");
+// }
+
+//Question no  06
+
+// var hour = 13;
+// var hour = prompt("Enter the hour?");
+// if (hour < 18) {
+// alert(greeting = "Good day");
+// }
+// else {
+// alert(greeting = "Good evening");
+// }
+
+//Question no 07
+
+// var time = 1900;
+// var hour = "x";
+// var time1 = "7pm";
+// time = time1;
+// var time = prompt("Enter the hour?");
+// if(time >= 0000 && time < 1200) {
+//     alert("Good Morning!");
+// }
+// else if(time >= 1200 && time < 1700) {
+//     alert("Good AfterNoon!");
+// }
+// else if(time >= 1700 && time < 2100) {
+//     alert("Good Evening!");
+// }
+// else if(time >= 2100 && time <= 2359) {
+//     alert("Good Night!");
+// }
+// else {
+//     alert("Invalid.");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
